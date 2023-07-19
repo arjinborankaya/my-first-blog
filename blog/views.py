a450314f064d45772b46bca8487c14dc3f4ee804
Arjin Boran Kaya <boran.kaya@ozu.edu.tr>
2023-07-19 15:52:04 +0300

    Changed a function based view to a class based view for testing.

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from django.views import View
 
 
 def post_list(request):
@@ -12,9 +13,11 @@ def post_list(request):
     return render(request, 'blog/post_list.html',{'posts': posts})
 
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+class post_detail(View):
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        return render(request, 'blog/post_detail.html', {'post': post})
+
 
 @login_required
 def post_new(request):
